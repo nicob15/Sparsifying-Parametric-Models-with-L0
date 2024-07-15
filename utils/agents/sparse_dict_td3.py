413dd5475ed2d0158780d9b187f6a160a4f3bd3c
nicob15 <nicolobotteghi15@gmail.com>
2024-07-15 17:24:08 +0200

    add different dictionary libraries in control example

--- utils/agents/sparse_dict_td3.py
+++ utils/agents/sparse_dict_td3.py
@@ -2,30 +2,34 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from sklearn.preprocessing import PolynomialFeatures
+
 from utils.l0_layer import L0Dense
 
+from utils.agents.feature_libraries.fourier_features import FourierLibrary
+from utils.agents.feature_libraries.polynomial_features import PolynomialLibrary
+from utils.agents.feature_libraries.generalized_features import GeneralizedLibrary
 import os
 
 # Implementation of Twin Delayed Deep Deterministic Policy Gradients (TD3)
 # Paper: https://arxiv.org/abs/1802.09477
 
 class Actor(nn.Module):
-    def __init__(self, coef_dim, max_action, poly, scale=1.0, action_dim=1, droprate=0.5, device='cuda'):
+    def __init__(self, features_dim, max_action, lib, scale=1.0, action_dim=1, droprate=0.5, device='cuda'):
         super(Actor, self).__init__()
 
-        self.actor_coef_1 = L0Dense(in_features=coef_dim, out_features=action_dim, bias=False, local_rep=False,
+        self.actor_coef_1 = L0Dense(in_features=features_dim, out_features=action_dim, bias=False, local_rep=False,
                                     droprate_init=droprate, weight_decay=0.0)
-
         self.max_action = max_action
-        self.poly = poly
-        self.scale = scale
-
+        self.lib = lib
+        self.scale_features = scale
         self.device = device
 
+        torch.nn.init.normal_(self.actor_coef_1.weights, mean=0, std=0.01)
+
     def forward(self, x):
-        p = torch.from_numpy(self.poly.fit_transform((x).cpu().numpy())).to(self.device)
-        a = self.actor_coef_1(p)
+        x = x / self.scale_features
+        xf = torch.from_numpy(self.lib.transform((x).cpu().numpy())).to(self.device)
+        a = self.actor_coef_1(xf)
         a = self.max_action * torch.tanh(a)
         return a
 
@@ -66,26 +70,78 @@ class Critic(nn.Module):
 
 class TD3(object):
     def __init__(self, state_dim, action_dim, max_action, degree_pi=2, feature_scale=1.0, h_dim=256, tau=0.005,
-                 reg_coeff=0.001, droprate=0.5, device='cuda'):
+                 reg_coeff=0.001, droprate=0.5, policy_type='polynomial', frequency=1, device='cuda'):
 
-        self.poly_pi = PolynomialFeatures(degree=degree_pi)
-        x = np.ones((1, state_dim))
-        p = self.poly_pi.fit_transform(x)
-        coef_dim = p.shape[1]
-        print("policy polynomial of order ", degree_pi)
-        print("with {} coefficients".format(coef_dim))
-        print(self.poly_pi.get_feature_names_out())
+        self.policy_type = policy_type
+        if policy_type == 'polynomial':
+            self.lib = PolynomialLibrary(degree=degree_pi, include_bias=True, include_interaction=True)
+            x = np.ones((1, state_dim))
+            self.lib.fit(x)
+            xp = self.lib.transform(x)
+            coef_dim = xp.shape[1]
+            print("############################")
+            print("policy polynomial of degree ", degree_pi)
+            print("with {} coefficients".format(coef_dim))
+            print(self.lib.get_feature_names())
 
-        self.actor = Actor(coef_dim, max_action, droprate=droprate, poly=self.poly_pi, scale=feature_scale,
-                           action_dim=action_dim, device=device).to(device)
-        self.actor_target = Actor(coef_dim, max_action, droprate=droprate, poly=self.poly_pi, scale=feature_scale,
-                                  action_dim=action_dim, device=device).to(device)
-        self.actor_target.load_state_dict(self.actor.state_dict())
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+            self.actor = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                               scale=feature_scale, action_dim=action_dim, device=device).to(device)
+
+            self.actor_target = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                                      scale=feature_scale, action_dim=action_dim, device=device).to(device)
+            self.actor_target.load_state_dict(self.actor.state_dict())
+
+            self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+
+
+        if policy_type == 'fourier':
+            self.lib = FourierLibrary(n_frequencies=frequency, include_sin=True, include_cos=True, interaction_terms=True)
+            x = np.ones((1, state_dim ))
+            self.lib.fit(x)
+            xf = self.lib.transform(x)
+            coef_dim = xf.shape[1]
+            print("############################")
+            print("fourier policy with frequency ", frequency)
+            print("with {} coefficients".format(coef_dim))
+            print(self.lib.get_feature_names())
+
+            self.actor = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                               scale=feature_scale, action_dim=action_dim, device=device).to(device)
+
+            self.actor_target = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                                      scale=feature_scale, action_dim=action_dim, device=device).to(device)
+            self.actor_target.load_state_dict(self.actor.state_dict())
+
+            self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+
+        if policy_type == "polyfourier":
+            poly_lib = PolynomialLibrary(degree=degree_pi, include_bias=True, include_interaction=True)
+            fourier_lib = FourierLibrary(n_frequencies=frequency, include_sin=True, include_cos=True, interaction_terms=True)
+            self.lib = GeneralizedLibrary([poly_lib, fourier_lib])
+            x = np.ones((1, state_dim))
+            self.lib.fit(x)
+            xpf = self.lib.transform(x)
+            coef_dim = xpf.shape[1]
+            print("############################")
+            print("polynomial with degree {} + fourier policy with frequency {} ".format(degree_pi, frequency))
+            print("with {} coefficients".format(coef_dim))
+            print(self.lib.get_feature_names())
+
+            self.actor = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                               scale=feature_scale, action_dim=action_dim, device=device).to(device)
+
+            self.actor_target = Actor(features_dim=coef_dim, max_action=max_action, droprate=droprate, lib=self.lib,
+                                      scale=feature_scale, action_dim=action_dim, device=device).to(device)
+            self.actor_target.load_state_dict(self.actor.state_dict())
+
+            self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+
 
         self.critic = Critic(state_dim, action_dim, h_dim).to(device)
+
         self.critic_target = Critic(state_dim, action_dim, h_dim).to(device)
         self.critic_target.load_state_dict(self.critic.state_dict())
+
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
 
         self.max_action = max_action
@@ -96,7 +152,7 @@ class TD3(object):
 
     def select_action(self, state):
         self.actor.eval()
-        self.actor.training = False
+        self.actor.actor_coef_1.training = False
         state = torch.FloatTensor(state.reshape(1, -1)).to(self.device)
         return self.actor(state).cpu().data.numpy().flatten()
 
@@ -105,12 +161,15 @@ class TD3(object):
         pass
 
     def print_equations(self, ep):
+        self.actor.eval()
+        self.actor.actor_coef_1.training = False
+
         w1 = self.actor.actor_coef_1.weights
         mask1 = self.actor.actor_coef_1.sample_z(1, sample=False)
         _, idx1 = torch.where(mask1 > 0.0)
         w1 = w1[idx1, :]
         mask1 = mask1[:, idx1]
-        coef1 = self.actor.poly.get_feature_names_out()
+        coef1 = np.array(self.actor.lib.get_feature_names())
         coef1 = coef1[idx1.cpu()]
         print("x0 = s1, x1 = s2, x2 = s3")
         print("pi is equal to:")
@@ -121,7 +180,7 @@ class TD3(object):
         print("weight")
         print(w1.reshape(1, -1))
 
-        with open(os.path.join('figures/policy.txt'), 'a') as file:
+        with open(os.path.join('figures/' + self.policy_type + '_policy.txt'), 'a') as file:
             file.write("\n")
             file.write("\n")
             file.write("Policy at training episode {}".format(ep))
@@ -135,12 +194,12 @@ class TD3(object):
         file.close()
 
     def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, policy_noise=0.2, noise_clip=0.3,
-              policy_freq=2, clipped_noise=False,):
+              policy_freq=2, clipped_noise=False):
 
-        self.critic.train()
-        self.actor.train()
-        self.actor.training = True
         for it in range(iterations):
+            self.critic.train()
+            self.actor.train()
+            self.actor.actor_coef_1.training = True
 
             # Sample replay buffer
             x, u, y, r, d = replay_buffer.sample(batch_size)
